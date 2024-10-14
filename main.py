580ea1665bd94be726186f0c267e3f9c4de5f337
parkhomenko-alexander <parkhomenko.alx@gmail.com>
2024-10-14 11:33:21 +1000

    init

--- main.py
+++ main.py
@@ -52,5 +52,6 @@ def start_server(host='0.0.0.0', port=2020):
         client_handler.start()
 
 if __name__ == "__main__":
+    print("hello work")
     # Start the Wialon server
     start_server(port=2020)
